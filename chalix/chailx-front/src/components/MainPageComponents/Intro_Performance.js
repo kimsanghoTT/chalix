2a9fa5707936abdb4cd18d0159e4279f0f1aadb5
김상호 <165999006+kimsanghoTT@users.noreply.github.com>
2025-03-02 18:58:19 +0900

    실적 안내 약간

--- chalix/chailx-front/src/components/MainPageComponents/Intro_Performance.js
+++ chalix/chailx-front/src/components/MainPageComponents/Intro_Performance.js
@@ -4,8 +4,19 @@ import "../../css/MainPage.css";
 const Intro_Performance = () => {
 
     return(
-        <div className="presentation-container">
-
+        <div className="performance-container">
+            <div className="performance-images">
+                <img src="/images/5(1).png"/>
+                <img src="/images/4(1).png"/>
+                <img src="/images/1(1).png"/>
+                <img src="/images/3(1).png"/>
+                <img src="/images/2(1).png"/>
+            </div>
+            <div className="performance-overlay-text">
+                <p>Environmental consultancy firm
+                offering high-value advisory services</p>
+                <button>사업실적 &nbsp;&nbsp;→</button>
+            </div>
         </div>
     )
 }
